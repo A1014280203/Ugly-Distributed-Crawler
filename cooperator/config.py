087d8ba4ce3588b044e1bd2c23256eda222dbb89
A1014280203 <1014280203@qq.com>
2017-03-31 15:32:51 +0800

    add notation

--- cooperator/config.py
+++ cooperator/config.py
@@ -1,10 +1,13 @@
+# settings for connecting redis-server
 r_server = {
     'ip': "1.2.3.4",
     'port': '6379',
     'passwd': '',
+    # the name of set which stores proxies
     's_name': 'proxy_ip'
 }
 
+# for time.sleep()
 settings = {
     'interval': 5*60
 }
